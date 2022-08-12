33d66e6bd15c6c99827be77f4c08010a1e8b94a6
maroshaj <catherinejohnj2@gmail.com>
2022-08-12 14:19:20 +0300

    created Medicine model

--- MedicineTrack/models.py
+++ MedicineTrack/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Medicine(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.CharField(max_length=400)
+    photo=  models.ImageField("Medicine photo",upload_to = 'pics/',null=True
+                             )
+    status=models.BooleanField(default=True)
