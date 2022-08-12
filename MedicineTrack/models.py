7ca9fae98cd2ab6a24aec258335bc9bd84e12854
shekina99 <shekinagaitan@gmail.com>
2022-08-12 14:20:04 +0300

    add status and order

--- MedicineTrack/models.py
+++ MedicineTrack/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Orders(models.Model):
+    retail_id=models.ForeignKey(Retail,on_delete=CASCADE)
+    order_date=models.DateTimeField()
+    status=models.BooleanField(default=True)
+
+    def_str_(self):
+    return self.retail_id:
+
+    class Status(models.Model):
+        status_name=models.CharField(default=True)
+        status=models.BooleanField()
+
+
+
