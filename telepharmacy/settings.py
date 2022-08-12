23e22ddacc91b53017c14f96d8de819865f98bb2
shekina99 <shekinagaitan@gmail.com>
2022-08-12 12:14:07 +0300

    new changes pulled

--- telepharmacy/settings.py
+++ telepharmacy/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'MedicineTrack',
-        'PASSWORD': 'Catherine@27', #Enter your postgres password
+        'PASSWORD': '12345', #Enter your postgres password
         'USER': 'postgres',
         'HOST': 'localhost'
     }
