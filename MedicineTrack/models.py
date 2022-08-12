d5595743f13114ddb619ae82fabbe5f07d36b181
Neila <neilakipigapasi@gmail.com>
2022-08-12 14:19:33 +0300

    installed django_countries in settings.py,added a model in models.py

--- MedicineTrack/models.py
+++ MedicineTrack/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django_countries.fields import CountryField
 # Create your models here.
+
+class Shipping(models.Model):
+    order_id=models.OneToOneField(Order,on_delete=models.CASCADE)
+    country= CountryField(blank=True)
+    CITIES=(
+        ('DOM','DODOMA'),
+        ('DSM','DAR-ES-SALAAM'),
+        ('ARS','ARUSHA'),
+        ('MBY','MBEYA'),
+        ('KLM','KILIMANJARO'),
+        ('BKB','BUKOBA'),
+        ('IRI','IRINGA'),
+        ('NJO','NJOMBE'),
+    )
+    city=models.CharField(max_length=3, choices=CITIES)
+    address=models.CharField(max_length=50)
+    email=models.EmailField(max_length=254)
+    phonenumber=models.CharField(max_length=30)
+
