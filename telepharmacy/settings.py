1d3f721159a613b4b5ae77e54df4b06290b1da79
Neila <neilakipigapasi@gmail.com>
2022-08-14 16:19:27 +0300

    created a new file

--- telepharmacy/settings.py
+++ telepharmacy/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'MedicineTrack',
-        'PASSWORD': 'Catherine@27', 
+        'PASSWORD': 'ARVYN1', 
         'USER': 'postgres',
         'HOST': 'localhost'
     }
