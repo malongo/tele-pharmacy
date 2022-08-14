f00e3eb52868550724c2a063f836c98cb2da0b46
Neila <neilakipigapasi@gmail.com>
2022-08-14 16:40:11 +0300

    edited a new file

--- MedicineTrack/urls.py
+++ MedicineTrack/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+import MedicineTrack
+
+app_name="MedicineTrack"
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('shipping',views.shipping, name="shipping"),
 ]
--- MedicineTrack/views.py
+++ MedicineTrack/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .models import Shipping
+from .forms import FormShipping
 
 # Create your views here.
+ship=Shipping.objects.all()
+
 def index(request):
     return render(request, 'base.html')
+def shipping(request): 
+    if request.method == 'POST':
+        form=FormShipping(request.POST or None,request.FILES)
+        if form.is_valid():
+
+         form.save()
+         #return redirect('baraka:famm')
+    else:
+        form = FormShipping()
+    return render(request,'shipping.html',{'form':form})
