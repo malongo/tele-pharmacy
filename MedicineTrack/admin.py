d1e1965d5bdc091eef12f3c04bbb69ae343684a9
TFKprogrammer <rogerskarunde@gmail.com>
2022-08-15 10:17:33 +0300

    adding boostrap folder charts  js and templates

--- MedicineTrack/admin.py
+++ MedicineTrack/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Status,OrderStatus,Medicine,Retail,OrderMedicine,Order,MedicinePrice,Shipping
 
 # Register your models here.
+admin.site.register([Status,OrderStatus,Medicine,Retail,OrderMedicine,Order,MedicinePrice,Shipping])
--- MedicineTrack/urls.py
+++ MedicineTrack/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
 from . import views
 
+app_name = "MedicineTrack"
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.store, name='store'),
+    path('login', views.login_request, name='login'),
+    path('register', views.register, name='register'),
+    path('logout', views.logout_request, name='logout'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+    path('statistics', views.statistics, name='statistics'), 
+    path('order', views.order, name='order'), 
+    path('cart', views.cart, name="cart"),
+    path('checkout', views.checkout, name="checkout"),
+    path('update_item/', views.updateItem, name="update_item"),
 ]
