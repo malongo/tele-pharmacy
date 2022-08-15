425c0deba38b51310362e5e06066700cdf498194
princessvi <vailetadolph@gmail.com>
2022-08-15 15:58:28 +0300

    commited

--- MedicineTrack/urls.py
+++ MedicineTrack/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('Retaildetails', views.RetailDetails, name="RetailDetails"),
     path('addretail', views.addRetail, name="addRetail"),
     path('contact', views.contact, name="contact"),
